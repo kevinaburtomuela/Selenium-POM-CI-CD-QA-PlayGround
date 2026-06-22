0f10afb63c4327bce1d6b3dd4febaf69656d060e
KevinAburto <kevinaburtomuela@hotmail.com>
2026-06-22 17:18:38 -0600

    Fix: Github actions error

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
+import os
 import pytest
 from selenium import webdriver
 from pages.login_page import LoginPage
+from selenium.webdriver.chrome.options import Options
 
 USERS = {
     "admin": ("admin", "admin123"),
@@ -9,9 +11,21 @@ USERS = {
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+    options = Options()
+
+    if os.getenv("CI"):
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=options)
+
+    if not os.getenv("CI"):
+        driver.maximize_window()
+
     yield driver
+
     driver.quit()
 
 @pytest.fixture
